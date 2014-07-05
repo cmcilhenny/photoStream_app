0f8c2a5e57e31164e854f5319efe8b4fa0fb69fd
courtney mcilhenny <github@courtneymcilhenny>
2014-07-05 14:46:22 -0700

    updating model, removing notes.

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,8 +7,6 @@ class Event < ActiveRecord::Base
   validates :custom_url, presence: true
   validates_uniqueness_of :custom_url
 
-  # TODO: fix date validation and rspec
-
   # TODO: look into how uniqueness works. Uniqueness should only apply to both name and date. You should be able to have multiple events on one date and muliple events with the same name on different dates. Look up scope validation
 
   has_many :photos
